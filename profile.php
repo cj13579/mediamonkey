71783c64276c90924d58160aa03655f2e3baca86
cj13579 <cjb.blake@gmail.com>
2014-04-30 15:28:13 +0100

    Temp bubble for subscription functionality

--- profile.php
+++ profile.php
@@ -207,6 +207,7 @@ exit;
       	<div class="row">
       		<h2>Subscriptions</h2>
       		<p>Use the following form to subscribe to TV shows and movies. When you are subscribed you will be emailed when a new episode of that show or a new movie is added to the library. Just save your email address in the section above.</p>
+      	<div class="alert alert-info">You can register for TV shows and Movies but this functionality is not yet fully automated!</div>
       	</div>
       	
       	<div class="row">
